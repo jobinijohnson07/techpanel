bf7692937f2332325d927143157809e60549585e
jobinijohnson07 <jobinijohnson981@gmail.com>
2022-08-19 19:51:29 +0530

    UI and collapse done

--- src/widgets/kanbanView/card/card.js
+++ src/widgets/kanbanView/card/card.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import { useState } from 'react';
 import './card.scss'
 import Google from '../../../Assets/google.png';
 import Tata from '../../../Assets/tata.png';
 import Orange from '../../../Assets/orange.svg';
 import Green from '../../../Assets/green.svg';
-import { DragSource, DropTarget } from "react-dnd";
-import { useState } from 'react';
+import Wishlist from '../../../Assets/wishlist.svg';
+import WishlistOpen from '../../../Assets/wishlistopen.svg';
+import Recommended from '../../../Assets/recommended.svg';
 import Arrow from "../../../Assets/arrow_dropdown.svg";
+import { DragSource, DropTarget } from "react-dnd";
 import _ from "lodash";
 
 // function Card(props){
@@ -25,12 +27,34 @@ import _ from "lodash";
 function Card(props){
     console.log("props", props)
     const [card, setCard] = useState(props.data);
+    const [isWishIcon, setWishIcon] = useState(true)
+    const [isWishlistIcon, setWishlistIcon] = useState(false)
+    const [isRecommendedOpen, setRecommendedOpen] = useState(false);
     // function expand(item, index){
     //     let arr = card;
     //     arr.cardList[index].expanded = !arr.cardList[index].expanded;
     //     setCard(arr);
     //     console.log(card.cardList, ' index');
     // }
+
+    const handleIswishIcon = ()=> {
+        setWishIcon(false);
+        setWishlistIcon(true);
+     }
+  
+     const handleIsWishlistIcon = ()=> {
+        setWishIcon(true);
+        setWishlistIcon(false);
+     } 
+
+     const handleIsRecommendedIcon = () => {
+        setRecommendedOpen(true);
+     }
+
+     const handleIsRecommendedOpen = () => {
+        setRecommendedOpen(false);
+     }
+
     return(
       <div className="card-section">       
         {card.cardList.map((item,key)=>{
@@ -38,7 +62,28 @@ function Card(props){
            <>
             <div className="card-content" key={key}>
                 <button data-bs-toggle="collapse" className='expand-button' data-bs-target={`#collapse${item.id.toString()}`}> <img src={Arrow} alt="Arrow"/> </button>
-              <div className="name-content">{item.name}</div>
+              <div className="d-flex">
+               <div className="name-content">{item.name}</div>
+               <div className="d-flex whole-content">
+                 <div className="wishlist-content">
+                   {item.wishlist.map((wishlist) => (
+                        <div className='col-5 p-0 m-0'>
+                            {/* {isWishIcon &&
+                              <img className="wishlist" src={Wishlist} alt="wishlist" onClick={handleIswishIcon} />
+                            }
+                            {isWishlistIcon &&
+                            <img className="wishlist" src={WishlistOpen} alt="wishlistOpen" onClick={handleIsWishlistIcon} />
+                            } */}
+                            <span >{wishlist === 'false' ? <img src={Wishlist} alt='wishlist'/> : <img src={WishlistOpen} alt='wishlistOpen'/> }</span>
+                        </div>
+                    ))}
+                 </div>
+                 <div className="recommendedimg-content" onClick={handleIsRecommendedIcon}><img src={Recommended} alt="Recommended" /></div>
+                 {isRecommendedOpen &&
+                   <div className="recommendedwhole-content" onClick={handleIsRecommendedOpen}>Remove Recommendation</div>
+                 }
+               </div>
+              </div>
               <div className="position-content">{item.position} - {item.company} ({item.place})</div>         
                 <div className="panel-content">{item.panel.length > 0 ? 
                     <div>Panel on                
--- src/widgets/kanbanView/column/column.js
+++ src/widgets/kanbanView/column/column.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react';
 import './column.scss';
 import Card from '../card/card';
 import Filter from '../../../Assets/filter.svg';
@@ -6,16 +6,63 @@ import Sort from '../../../Assets/sort.svg';
 import Recommended from '../../../Assets/recommended.svg';
 
 function Column({data}) {
+    const [isFilterOpen, setFilterOpen] = useState(false);
+    const [isSortOpen, setSortOpen] = useState(false);
+    const [isPreferencesOpen, setPreferencesOpen] = useState(false);
     // console.log(data, " data");
+
+    const handleIsFilterIcon = () => {
+      setFilterOpen(true);
+    }
+
+    const handleIsFilterOpen = () => {
+      setFilterOpen(false);
+    }
+
+    const handleIsSortIcon = () => {
+      setSortOpen(true);
+    }
+
+    const handleIsSortOpen = () => {
+      setSortOpen(false);
+    }
+
+    const handleIsPreferencesIcon = () => {
+      setPreferencesOpen(true);
+    }
+
+    const handleIsPreferencesOpen = () => {
+      setPreferencesOpen(false);
+    }
+
     return(
         <div className="column-section">
           <div className="data-section">
             <div className="d-flex">
               <div className="title-content">{data.title}</div>
               <div className="d-flex img-content">
-                <div className="filterimg-content"><img src={Filter} alt="Filter" /></div>
-                <div className="filterimg-content"><img src={Sort} alt="Sort" /></div>
-                <div><img src={Recommended} alt="Recommended" /></div>
+                <div className="filterimg-content" onClick={handleIsFilterIcon}><img src={Filter} alt="Filter" /></div>
+                {isFilterOpen &&
+                  <div className="filterwhole-content" onClick={handleIsFilterOpen}>
+                    <div className="filterheading-content">Sort by</div>
+                    <div className="filter-content">Relevant</div>
+                    <div className="filter-content">A to Z</div>
+                    <div className="filter-content">Z to A</div>
+                  </div>
+                }
+                <div className="filterimg-content" onClick={handleIsSortIcon}><img src={Sort} alt="Sort" /></div>
+                {isSortOpen &&
+                  <div className="sortwhole-content" onClick={handleIsSortOpen}>
+                    <div className="filter-content">Favourites</div>
+                    <div className="filter-content">Save Search</div>
+                  </div>
+                }
+                <div className="preferencesimg-content" onClick={handleIsPreferencesIcon}><img src={Recommended} alt="Preferences" /></div>
+                {isPreferencesOpen &&
+                  <div className="preferenceswhole-content" onClick={handleIsPreferencesOpen}>
+                    <div className="filter-content">Save Preferences</div>
+                  </div>
+                }
               </div>
             </div>
             
